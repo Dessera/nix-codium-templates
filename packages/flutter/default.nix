4f07edc879bc722478e38cf4fcfd3bc499ae8e5d
Dessera <1533653159@qq.com>
2024-06-07 18:29:30 +0800

    change old packages format

--- packages/flutter/default.nix
+++ packages/flutter/default.nix
@@ -1,15 +1,18 @@
 { pkgs, codium-extensions }:
+
 let
-  cfg = ./settings.json;
-  codium = pkgs.vscode-with-extensions.override {
-    vscode = pkgs.vscodium;
-    vscodeExtensions = (with codium-extensions.vscode-marketplace; [
+  utils = import ../../utils;
+in
+utils.mkCode {
+  inherit pkgs;
+  settingsPath = ./settings.json;
+      vscodeExtensions = (with codium-extensions.vscode-marketplace; [
       igorsbitnev.error-gutters
       # github.copilot-labs
       github.copilot
       eamodio.gitlens
       mhutchie.git-graph
-    ]) ++ (with codium-extensions.open-vsx; [
+  ]) ++ (with codium-extensions.open-vsx; [
       # common
       ms-ceintl.vscode-language-pack-zh-hans
       usernamehw.errorlens
@@ -19,12 +22,5 @@ let
       # flutter
       dart-code.flutter
       dart-code.dart-code
-    ]);
-  };
-in
-pkgs.writeShellScriptBin "codium-run"
-  ''
-    mkdir -p $PWD/.code-user/User/
-    cp ${cfg} $PWD/.code-user/User/settings.json
-    ${codium}/bin/codium --user-data-dir=$PWD/.code-user $PWD $@
-  ''
+  ]);
+}
