7921da20a84cff500576794c0f283a775c7499f2
Dessera <1533653159@qq.com>
2024-06-07 16:59:35 +0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+    nix-vscode-extensions = { url = "github:nix-community/nix-vscode-extensions"; };
+    flake-parts = { url = "github:hercules-ci/flake-parts"; };
+    devshell = { url = "github:numtide/devshell"; };
+  };
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , nix-vscode-extensions
+    , flake-parts
+    , devshell
+    , ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ devshell.flakeModule ];
+      # Development shell
+      perSystem = { config, pkgs, system, ... }:
+        let
+          codium-extensions = nix-vscode-extensions.extensions."${system}";
+        in
+        rec {
+          formatter = pkgs.nixpkgs-fmt;
+          devshells.default = {
+            packages = with pkgs; [ nil nixpkgs-fmt packages.codium-nix ];
+          };
+
+          packages.codium-flutter = import ./packages/flutter {
+            inherit pkgs;
+            inherit codium-extensions;
+          };
+
+          packages.codium-nix = import ./packages/nix {
+            inherit pkgs;
+            inherit codium-extensions;
+          };
+        };
+    };
+}
---
+++ packages/nix/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, codium-extensions }:
+let
+  cfg = ./settings.json;
+  codium = pkgs.vscode-with-extensions.override {
+    vscode = pkgs.vscodium;
+    vscodeExtensions = (with codium-extensions.vscode-marketplace; [
+      igorsbitnev.error-gutters
+      github.copilot
+      eamodio.gitlens
+      mhutchie.git-graph
+    ]) ++ (with codium-extensions.open-vsx; [
+      # common
+      ms-ceintl.vscode-language-pack-zh-hans
+      usernamehw.errorlens
+      oderwat.indent-rainbow
+      zhuangtongfa.material-theme
+      vscode-icons-team.vscode-icons
+      # nix
+      jnoortheen.nix-ide
+    ]);
+  };
+in
+pkgs.writeShellScriptBin "codium-run"
+  ''
+    mkdir -p $PWD/.code-user/User/
+    cp ${cfg} $PWD/.code-user/User/settings.json
+    ${codium}/bin/codium --user-data-dir=$PWD/.code-user $PWD $@
+  ''
